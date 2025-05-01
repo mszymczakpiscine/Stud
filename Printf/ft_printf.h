25e58b03e02da11945e88bf007fa45bd7f7558da
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-05-01 12:45:56 +0200

    Add files via upload

--- Printf/ft_printf.h
+++ Printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 11:06:10 by mszymcza          #+#    #+#             */
-/*   Updated: 2025/04/29 11:13:16 by mszymcza         ###   ########.fr       */
+/*   Updated: 2025/05/01 12:00:22 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int ft_printf(const char *format, ...);
-int ft_putchar(char c);
-int ft_putstr(char *s);
-int ft_putnbr(int n);
-int ft_putnbr_unsigned(unsigned int n);
-int ft_puthex(unsigned int n, char format);
-int ft_putptr(unsigned long n);
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_putnbr_unsigned(unsigned int n);
+int	ft_puthex(unsigned int n, char format);
+int	ft_putptr(unsigned long n);
 
 #endif
--- Printf/utils/ft_putstr.c
+++ Printf/utils/ft_putstr.c
@@ -6,20 +6,21 @@
 /*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 19:44:09 by codespace         #+#    #+#             */
-/*   Updated: 2025/04/29 11:08:33 by mszymcza         ###   ########.fr       */
+/*   Updated: 2025/05/01 12:05:05 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
-    int i = 0;
+	int	i;
 
-    if (!s)
-        return (write(1, "(null)", 6));
-    while (s[i])
-        i++;
-    write(1, s, i);
-    return (i);
+	i = 0;
+	if (!s)
+		return (write(1, "(null)", 6));
+	while (s[i])
+		i++;
+	write(1, s, i);
+	return (i);
 }
