9b27401d6ebac3c1ee1b11f777332b067e1f7f45
mszymcza <mszymcza@student.42.fr>
2025-05-14 21:40:13 +0200

    miseajour

--- Get_next_line/main.c
+++ Get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 10:17:07 by mszymcza          #+#    #+#             */
-/*   Updated: 2025/05/13 18:14:22 by mszymcza         ###   ########.fr       */
+/*   Updated: 2025/05/14 21:40:03 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
-int	main()
+int	main(void)
 {
-	int	fd;
+	int		fd;
 	char	*line;
-	int		i = 0;
+	int		i;
 
+	i = 0;
 	fd = open("get_next_line.c", O_RDONLY);
 	if (fd < 0)
 	{
@@ -31,9 +32,10 @@ int	main()
 		line = get_next_line(fd);
 		if (!line)
 			break ;
-		printf("\e[43m%d|\e[0m\e[33m%s\e[0m\n", i++, line);
+		printf("\e[44m%d|\e[0m\e[38;5;213m%s\e[0m\n", i++, line);
 		free(line);
 	}
 	close(fd);
-	return 0;
+	return (0);
 }
+
