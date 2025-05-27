281d9c9fbd364cef428b61276d211a12f23a1787
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-05-27 22:06:07 +0200

    Update radix.c

--- push_swap_complete/src/radix.c
+++ push_swap_complete/src/radix.c
@@ -1,32 +1,44 @@
 
 #include "push_swap.h"
+#include <stdlib.h>
+
+static int compare_ints(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
 
 void assign_index(t_stack *a) {
     int size = stack_size(a);
     int *array = stack_to_array(a);
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (array[i] > array[j]) {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-        }
-    }
-    for (t_node *node = a->top; node; node = node->next)
-        for (int i = 0; i < size; i++)
-            if (node->value == array[i])
+
+    qsort(array, size, sizeof(int), compare_ints);
+
+    t_node *node = a->top;
+    while (node) {
+        int i = 0;
+        while (i < size) {
+            if (node->value == array[i]) {
                 node->index = i;
+                break;
+            }
+            i++;
+        }
+        node = node->next;
+    }
     free(array);
 }
 
 int *stack_to_array(t_stack *a) {
     int size = stack_size(a);
     int *array = malloc(sizeof(int) * size);
+    if (!array)
+        return NULL;
+
     t_node *tmp = a->top;
-    for (int i = 0; i < size; i++) {
+    int i = 0;
+    while (i < size && tmp) {
         array[i] = tmp->value;
         tmp = tmp->next;
+        i++;
     }
     return array;
 }
@@ -34,16 +46,24 @@ int *stack_to_array(t_stack *a) {
 void radix_sort(t_stack *a, t_stack *b) {
     int size = stack_size(a);
     int max_bits = 0;
-    while ((size - 1) >> max_bits)
+    int tmp = size - 1;
+
+    while (tmp >> max_bits)
         max_bits++;
-    for (int i = 0; i < max_bits; i++) {
-        for (int j = 0; j < size; j++) {
+
+    int i = 0;
+    while (i < max_bits) {
+        int j = 0;
+        int count = 0;
+        while (count < size) {
             if (((a->top->index >> i) & 1) == 0)
                 pb(a, b);
             else
                 ra(a);
+            count++;
         }
         while (!is_empty(b))
             pa(b, a);
+        i++;
     }
 }
