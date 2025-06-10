29fe73f63cbffc6f378e16d19cbef90495f4e3cc
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-06-10 13:52:30 +0200

    Add files via upload

--- exams/lvl2/wdmatch.c
+++ exams/lvl2/wdmatch.c
@@ -3,47 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   wdmatch.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mszymcza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/05 15:06:17 by mszymcza          #+#    #+#             */
-/*   Updated: 2025/06/05 15:14:00 by mszymcza         ###   ########.fr       */
+/*   Created: 2025/06/10 13:16:53 by mszymcza          #+#    #+#             */
+/*   Updated: 2025/06/10 13:29:00 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char *c)
-{
-	write(1, &c, 1);
-}
-void putstr(char *str)
-{
-	while (*str)
-		ft_putchar(*str++);
-}
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int i = 0;
 	int j = 0;
-	
+
 	if (argc == 3)
 	{
-		while (argv[2][i] != '\0')
+		while (argv[2][j] && argv[1][i])
 		{
-			if (argv[1][j] == '\0')
-			{
-				putstr(argv[1]);
-				ft_putchar('\n');
-				return 0;
-			}
-			if (argv[2][i] == argv[1][j])
-				j++;
-			i++;
-		}	
-		if (argv[1][j] == '\0')
-			putstr(argv[1]);
+			if (argv[1][i] == argv[2][j])
+				i++;
+			j++;
+		}
+		if (argv[1][i] == '\0')
+			write(1, argv[1], i);
 	}
-	ft_putchar('\n');
-	return 0;
+	write(1, "\n", 1);
 }
