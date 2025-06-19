5c9a6fc94dcbd6e910ef2d3f7ccdfcf057bace95
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-06-19 15:19:15 +0200

    Add files via upload

---
+++ pipex/libft/ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 19:47:10 by marvin            #+#    #+#             */
+/*   Updated: 2025/04/20 13:47:10 by mszymcza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!haystack || !needle)
+		return (NULL);
+	if (!needle[0])
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && haystack[i + j] && i + j < len)
+		{
+			j++;
+			if (!needle[j])
+				return ((char *)haystack + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
