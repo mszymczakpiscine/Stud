dc08557b74226e1a9da877fb74dd9ba81de89dd2
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-07-03 10:40:39 +0200

    Add files via upload

--- exams/lvl2/snake_camel.c
+++ exams/lvl2/snake_camel.c
@@ -5,39 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/05 15:17:05 by mszymcza          #+#    #+#             */
-/*   Updated: 2025/06/05 15:30:07 by mszymcza         ###   ########.fr       */
+/*   Created: 2025/07/03 10:31:03 by mszymcza          #+#    #+#             */
+/*   Updated: 2025/07/03 10:39:00 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	toupper(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (c - 32);
-	return (c);
-}
-
-int	ft_putchar(char c)
-{
-	write (1, &c , 1);
-}
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int i = 0;
-	
+	int upper = 0;
+	char c;
 	if (argc == 2)
 	{
 		while (argv[1][i])
 		{
 			if (argv[1][i] == '_')
-				ft_putchar(toupper(argv[1][++i]));
+				upper = 1;
 			else
-				ft_putchar(argv[1][i]);
+			{
+				if (upper && argv[1][i] >= 'a' && argv[1][i] <= 'z')
+				{
+					c = argv[1][i] - 32;
+					write(1, &c, 1);
+				}
+				else 
+					write(1, &argv[1][i], 1);
+				upper = 0;
+			}
 			i++;
 		}
 	}
-	ft_putchar('\n');
+	write(1, "\n", 1);
 }
