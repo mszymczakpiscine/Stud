6e9b4292f21b191d0495bf023a79ea4332122068
mszymczakpiscine <maiaszymczak98@gmail.com>
2025-07-03 11:32:33 +0200

    Add files via upload

--- exams/lvl2/ft_range.c
+++ exams/lvl2/ft_range.c
@@ -1,16 +1,39 @@
-int *ft_range(int start, int end)
-{
-    int i = 0;
-    int len = (end - start) < 0 ? ((end - start) * -1) + 1 : (end - start) + 1;
-    int *range = (int *) malloc(len * sizeof(int));
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 11:26:02 by mszymcza          #+#    #+#             */
+/*   Updated: 2025/07/03 11:32:27 by mszymcza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    while (i < len)
-    {
-        if (start < end)
-            range[i] = start++;
-        else
-            range[i] = start--;
-        i++;
-    }
-    return (range);
+#include <stdlib.h>
+
+int	*ft_range(int start, int end)
+{
+	int size;
+	int *range;
+	int i = 0;
+	if (start <= end)
+		size = end - start + 1;
+	else 
+		size = start - end + 1;
+	range = malloc(sizeof(int) * size);
+	if (!range)
+		return NULL;
+	if (start <= end)
+	{
+		while (start <= end)
+			range[++i] = start++;
+	}
+	else 
+	{
+		while (start >= end)
+			range[++i] = start--;
+	}
+	return range;
 }
+
