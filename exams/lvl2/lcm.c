5656bff7b458b37eb95d372f23f7105b5ad1dca2
maiasz <maiaszymczak98@gmail.com>
2025-07-10 14:36:51 +0200

    Add files via upload

--- exams/lvl2/lcm.c
+++ exams/lvl2/lcm.c
@@ -1,13 +1,48 @@
-unsigned int lcm(unsigned int a, unsigned int b)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lcm.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 17:42:52 by mszymcza          #+#    #+#             */
+/*   Updated: 2025/07/09 17:42:52 by mszymcza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+
+unsigned int	lcm(unsigned int a, unsigned int b)
 {
-    unsigned int g = (a > b) ? a : b;
-    
-    if (a == 0 || b == 0)
-    	return (0);
-    while (1)
-    {
-        if ((g % a == 0) && (g % b == 0))
-            return (g);
-        g++;
-    }
+	int i = 1;
+	int j = 1;
+
+	if (a == 0 || b == 0)
+		return 0;
+	while (1)
+	{
+		if ((i * a) == (j * b))
+			return (i * a);
+		if (i * a > j * b)
+		{
+			if ((UINT_MAX - (j * b)) < b)
+				return 0;
+			j++;
+		}
+		else if (i * a < j * b)
+		{
+			if ((UINT_MAX - (i * a)) < a)
+				return 0;
+			i++;
+		}
+	}
 }
+/*
+int	main()
+{
+	unsigned int result = lcm(-1, 5);
+	printf("%u", result);
+}
+*/
