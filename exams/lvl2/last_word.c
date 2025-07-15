4c48a66f668a1b300c144bfc1f57273b18187cab
maiasz <maiaszymczak98@gmail.com>
2025-07-15 11:23:29 +0200

    Add files via upload

--- exams/lvl2/last_word.c
+++ exams/lvl2/last_word.c
@@ -5,37 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/05 14:22:07 by mszymcza          #+#    #+#             */
-/*   Updated: 2025/06/05 14:53:33 by mszymcza         ###   ########.fr       */
+/*   Created: 2025/07/15 11:13:59 by mszymcza          #+#    #+#             */
+/*   Updated: 2025/07/15 11:13:59 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
 int	main(int argc, char **argv)
 {
 	int i = 0;
-	int in_word = 0;
-	int first_word = 1;
-
 	if (argc == 2)
 	{
 		while (argv[1][i])
-		{
-			if (argv[1][i] == ' ' || argv[1][i] == '\t')
-				in_word = 0;
-			else
-			{
-				if (!in_word)
-				{
-					if (!first_word)
-						write(1, " ", 1);
-					first_word = 0;
-				}
-				write(1, &argv[1][i], 1);
-				in_word = 1;
-			}
 			i++;
-		}
+		i--;
+		while (argv[1][i] <= 32)
+			i--;
+		while (argv[1][i] > 32)
+			i--;
+		i++;
+		while (argv[1][i] > 32)
+			write(1, &argv[1][i++], 1);
 	}
 	write(1, "\n", 1);
 }
