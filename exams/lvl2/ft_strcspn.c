49aefd46a7b2d413cdd360f5e50e5d0cde72e445
maiasz <maiaszymczak98@gmail.com>
2025-07-17 14:33:16 +0200

    Add files via upload

--- exams/lvl2/ft_strcspn.c
+++ exams/lvl2/ft_strcspn.c
@@ -5,32 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/05 13:33:51 by mszymcza          #+#    #+#             */
-/*   Updated: 2025/06/05 13:45:04 by mszymcza         ###   ########.fr       */
+/*   Created: 2025/07/11 12:23:45 by mszymcza          #+#    #+#             */
+/*   Updated: 2025/07/11 12:23:45 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-#include <unistd.h>
+#include <stdio.h>
 
 size_t	ft_strcspn(const char *s, const char *reject)
 {
 	int i = 0;
 	int j = 0;
-
-	while (s[i++])
+	while(s[i])
 	{
-		while (reject[j])
+		j = 0;
+		while(reject[j])
 		{
-			if (s[i] == reject[j++])
-				return (i);
+			if (reject[j++] == s[i])
+				return i;
 		}
+		i++;
 	}
 	return i;
 }
-int main()
-{
-	char *s = "hello";
-	const char *reject = "jfkhpell";
-	printf("%zu\n", ft_strcspn(s, reject));
-}
+//
+// int	main()
+// {
+// 	char *s = "test";
+// 	const char *reject = "es";
+// 	printf("%zu\n", ft_strcspn(s, reject));
+// }
