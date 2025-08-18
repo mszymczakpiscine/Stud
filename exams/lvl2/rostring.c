1d63393cf95d182b101e93b1d4dc2b206e02b142
maiasz <maiaszymczak98@gmail.com>
2025-08-18 15:19:23 +0200

    Update rostring.c

--- exams/lvl2/rostring.c
+++ exams/lvl2/rostring.c
@@ -12,40 +12,45 @@
 
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int i = 0;
-	int j = 0;
-	char *str;
+    int i = 0;
+    int j = 0;
+    int first = 0;
 
-	if (argc >= 2)
-	str = argv[1];
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	j = i;
-	while (str[i])
-	{
-		while (str[i] && str[i] != ' ' && str[i] != '\t')
-			i++;
-		while (str[i] && (str[i] == ' ' || str[i] == '\t'))
-			i++;
-		while ((str[i] && str[i] != ' ' && str[i] != '\t') && (str[i - 1] == ' ' || str[i - 1] == '\t'))
-		{
-			while (str[i] && str[i] != ' ' && str[i] != '\t')
-			{
-				write(1, &str[i], 1);
-				i++;
-			}
-			write(1, " ", 1);
-			i++;
-		}
-	}
-	while (str[j] && str[j] != ' ' && str[j] != '\t')
-	{
-		write(1, &str[j], 1);
-		j++;
-	}
-	write(1, "\n", 1);
-	return (0);
+    if (argc > 1)
+    {
+        char *str = argv[1];
+        while (str[i] == ' ' || str[i] == '\t')
+            i++;
+        j = i;
+        while (str[i] && str[i] != ' ' && str[i] != '\t')
+            i++;
+        while (str[i])
+        {
+            while (str[i] == ' ' || str[i] == '\t')
+                i++;
+            if (str[i])
+            {
+                if (first)
+                    write(1, " ", 1);
+                while (str[i] && str[i] != ' ' && str[i] != '\t')
+                {
+                    write(1, &str[i], 1);
+                    i++;
+                }
+                first = 1;
+            }
+        }
+        if (first)
+            write(1, " ", 1);
+        while (j < i && str[j] && str[j] != ' ' && str[j] != '\t')
+        {
+            write(1, &str[j], 1);
+            j++;
+        }
+    }
+    write(1, "\n", 1);
+    return 0;
 }
 
