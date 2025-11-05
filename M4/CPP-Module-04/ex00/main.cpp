4db6842f52494ebd8d4b1c4569f0495d15fd46f8
Maia Szymczak <mszymcza@f1r12s13.clusters.42paris.fr>
2025-11-05 14:25:39 +0100

    ex00

--- M4/CPP-Module-04/ex00/main.cpp
+++ M4/CPP-Module-04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mszymcza <mszymcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 11:11:41 by mszymcza          #+#    #+#             */
-/*   Updated: 2025/11/05 12:24:53 by mszymcza         ###   ########.fr       */
+/*   Updated: 2025/11/05 13:58:19 by mszymcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,42 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
+int	main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+	delete i;
+	delete j;
+	delete meta;
+
+	const Animal* meta = new Animal("Animal");
+	const Animal* j = new Dog("Snoopy");
+	const Animal* i = new Cat("Garfield");
+	
+	const Animal unknown("Unknown");
+	const Dog chien("Bill");
+	const Cat chat("Felix");
+	
+	chien.makeSound();
+	chat.makeSound();
+	unknown.makeSound();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+	delete i;
+	delete j;
+	delete meta;
+	
+	const WrongAnimal* meta = new WrongAnimal();
+	const WrongAnimal* i = new WrongCat("Wrong")
+}
